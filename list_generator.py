01c25e67c26ab7a7d82b0428a97301a6ae090d0c
BalduinLandolt <balduin.landolt@unibas.ch>
2019-01-31 23:31:23 +0100

    added schema and dtd. runs now.
    issue with entities!

---
+++ list_generator.py
@@ -0,0 +1,54 @@
+import os
+import lxml
+from os import listdir
+from lxml import etree
+
+
+def get_by_name(name):
+    print("trying to load data by name ("+name+")...")
+    prefix = "input/xml/"
+    sysNo = ""
+    try:
+        root = etree.parse(prefix + name).getroot()
+        print(etree.tostring(root))
+        sysNo = root.get("catalogue_id")
+        print(sysNo)
+        print("got file by name.\n")
+    except OSError:
+        print("Could not read File: " + name + "\n")
+    except:
+        print("Syntax Issue!?!")
+    return sysNo
+
+
+def create_list_of_existing_system_numbers():
+    if os.path.isfile("./input/existing_numbers.txt"):
+        print("output file already exists.\nabort.")
+        return
+
+    print("getting a list of the contents of the directory...")
+    files = listdir("input/xml")
+    print("found:")
+    print(files)
+    print()
+    res = ""
+    for file in files:
+        print(file)
+        sys_no = get_by_name(file.__str__())
+        res += sys_no + "\n"
+
+    print("Result: ")
+    print(res)
+    with open("input/existing_numbers.txt", "w") as out_file:
+        print(res, file=out_file)
+
+    print("handled all files in folder.")
+
+
+
+def load_all_numbers():
+    print("loading numbers...")
+    res = []
+    with open("input/all_numbers.txt") as f:
+        res = f.readlines()
+    print("found numbers: " + str(res.__sizeof__()))
---
+++ main.py
@@ -0,0 +1,7 @@
+import list_generator
+
+print("Running...")
+
+list_generator.create_list_of_existing_system_numbers()
+
+print("Finished.")
